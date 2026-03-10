02fe4147a696bb42048be88a7052798b3b3c017b
sourav8476 <souravbosu2178@gmail.com>
2026-03-10 12:52:22 +0600

    Update config.py

--- config.py
+++ config.py
@@ -60,10 +60,10 @@ https://dashboard.shareus.io/
 # TRUE for yes FALSE if no 
 USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
 # only shareus service known rightnow rest you can test on your own
-SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "arolinks.com")
+SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "linkshotner.shop")
 # SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "")
 #use this key if not working ☠️ (jokin!!)
-SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "9dd988a0552a97f798e7a811622c95b5a307dcb5")
+SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "e7beb3c8f756dfa15d0bec495abc65f58c0dfa95")
 #add your custom time in secs for shortlink expiration.
 # 24hr = 86400
 # 12hr = 43200
@@ -142,4 +142,5 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
 
